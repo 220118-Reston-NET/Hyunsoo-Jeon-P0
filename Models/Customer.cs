2c520883f1cfba45da67899d2d89f21bcbcab499
Hyunsoo Jeon <hsprime85@gmail.com>
2022-02-02 08:23:38 -0800

    add customerID

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,5 +1,7 @@
 namespace Models{
     public class Customer{
+        
+        public int CustomerID { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
         public string Email { get; set; }
