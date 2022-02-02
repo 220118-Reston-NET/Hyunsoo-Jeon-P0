a9af7eeccd4455852da1bd0009c854cb470f5798
Hyunsoo Jeon <hsprime85@gmail.com>
2022-02-02 11:38:39 -0800

    Added connect to DB

--- StoreUI/Program.cs
+++ StoreUI/Program.cs
@@ -2,9 +2,18 @@
 using StoreUI;
 using BL;
 using DL;
+using Microsoft.Extensions.Configuration;
 
 Log.Logger = new LoggerConfiguration()
     .WriteTo.File("./logs/user.txt").CreateLogger();
+
+var configuration = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json")
+        .Build();
+
+string _connectionString = configuration.GetConnectionString("Reference2DB");
+
 
 bool repeat = true;
 IMenu menu = new MainMenu();
@@ -17,19 +26,19 @@ while(repeat){
     switch(ans){
         case "SearchProduct":
             Log.Information("Displaying Search product Menu to user");
-            menu = new SearchProductMenu(new ProductBL(new Repository()));
+            menu = new SearchProductMenu(new ProductBL(new SQLRepository(_connectionString)));
             break;
         case "SearchCustomer":
             Log.Information("Displaying Search Customer Menu to user");
-            menu = new SearchCustomerMenu(new CustomerBL(new Repository()));
+            menu = new SearchCustomerMenu(new CustomerBL(new SQLRepository(_connectionString)));
             break;
         case "AddProduct":
             Log.Information("Displaying Adding product Menu to user");
-            menu = new AddProductMenu(new ProductBL(new Repository()));
+            menu = new AddProductMenu(new ProductBL(new SQLRepository(_connectionString)));
             break;
         case "AddCustomer":
             Log.Information("Displaying Adding customer Menu to user");
-            menu = new AddCustomerMenu(new CustomerBL(new Repository()));
+            menu = new AddCustomerMenu(new CustomerBL(new SQLRepository(_connectionString)));
             break;
         case "MainMenu":
             menu = new MainMenu();
